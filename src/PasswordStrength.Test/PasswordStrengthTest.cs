3fae0968e7b7677e90114f1257b1c38d28a3ef5b
Sayoan Oliveira <sayoan@gmail.com>
2019-05-07 08:42:31 -0300

    Stage

--- src/PasswordStrength.Test/PasswordStrengthTest.cs
+++ src/PasswordStrength.Test/PasswordStrengthTest.cs
@@ -12,9 +12,44 @@ namespace PasswordStrength.Test
         }
 
         [Fact]
-        public void Test_FilledPassword_True()
+        public void Test_FilledPassword_SizeLess8()
         {
             bool isPasswordStrength = PasswordStrength.IsStrongPassword("123456");
+            Assert.False(isPasswordStrength);
+        }
+
+        [Fact]
+        public void Test_FilledPassword_SizeThen8()
+        {
+            bool isPasswordStrength = PasswordStrength.IsStrongPassword("1234567A");
+            Assert.True(isPasswordStrength);
+        }
+
+        [Fact]
+        public void Test_FilledPassword_SizeMax16_False()
+        {
+            bool isPasswordStrength = PasswordStrength.IsStrongPassword("12345678123456789");
+            Assert.False(isPasswordStrength);
+        }
+
+        [Fact]
+        public void Test_FilledPassword_SizeMax16_True()
+        {
+            bool isPasswordStrength = PasswordStrength.IsStrongPassword("123456789012345A");
+            Assert.True(isPasswordStrength);
+        }
+
+        [Fact]
+        public void Test_OnlyNumbers_False()
+        {
+            bool isPasswordStrength = PasswordStrength.IsStrongPassword("12345678");
+            Assert.False(isPasswordStrength);
+        }
+
+        [Fact]
+        public void Test_UpperCase()
+        {
+            bool isPasswordStrength = PasswordStrength.IsStrongPassword("12345678aA");
             Assert.True(isPasswordStrength);
         }
     }
--- src/PasswordStrength/PasswordStrength.cs
+++ src/PasswordStrength/PasswordStrength.cs
@@ -6,7 +6,41 @@ namespace PasswordStrength
     {
         public static bool IsStrongPassword(string password)
         {
-            return password != string.Empty;
+            if (password == string.Empty)
+            {
+                return false;
+            }
+            else if (password.Length < 8 || password.Length > 16)
+            {
+                return false;
+            }
+            else if (!hasNumberAndLetters(password))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool hasNumberAndLetters(string password)
+        {
+            char[] caracteres = password.ToCharArray();
+            bool contemLetraMaiuscula = false;
+            bool contemNumero = false;
+
+            foreach (var caracter in caracteres)
+            {
+                int car = (int)caracter;
+                //Letra maiuscula
+                if (car >= 65 && car <= 90)
+                    contemLetraMaiuscula = true;
+                //Numero
+                if (car >= 48 && car <= 57)
+                    contemNumero = true;
+            }
+
+            return contemLetraMaiuscula && contemNumero;
+
         }
     }
 }
